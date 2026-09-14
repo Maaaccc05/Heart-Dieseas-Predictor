c5bcf7e9fb58523c100fe5bd04d33aa1b91c1fd4
Mayuresh Kamble <mackam30@gmail.com>
2026-09-14 18:38:07 +0530

    project is almost completed just some errors sorting is needed

--- app.py
+++ app.py
@@ -10,30 +10,34 @@ st.title("Heart Stroke Prediction")
 st.markdown("Provide the following Details")
 
 age = st.slider("Age", 18, 100, 40)
-sex = st.selectbox("Sex",['M','F'])
-chestPain = st.selectbox("Chest Pain Type", ["ATA"], ["NAP"], ["TA"], ["ASY"])
-restingBP = st.number_input("Resting BP (mm, Hg)",80, 200, 120)
+sex = st.selectbox("Sex", ["M", "F"])
+chestPain = st.selectbox("Chest Pain Type", ["ATA", "NAP", "TA", "ASY"])
+restingBP = st.number_input("Resting BP (mm, Hg)", 80, 200, 120)
 cholestrol = st.number_input("Cholestrol (mg/dL)", 100, 600, 200)
-fastingBS = st.selectbox("Fasting Blood Sugar > 120 mg/dL",[0, 1] )
+fastingBS = st.selectbox("Fasting Blood Sugar > 120 mg/dL", [0, 1])
 restingECG = st.selectbox("Resting ECG", ["Normal", "ST", "LVH"])
-maxHR = st.slider("Max Heart Rate")
-exercise_angina = st.selectboxO("Exercise-Induced Angina", ["Y", "N"])
-old_peak = st.slider("Oldpeak (ST Depression)", 0.0, 6.0, 1.0 )
+maxHR = st.slider("Max Heart Rate", 60, 220, 150)
+exercise_angina = st.selectbox("Exercise-Induced Angina", ["Y", "N"])
+old_peak = st.slider("Oldpeak (ST Depression)", 0.0, 6.0, 1.0)
 st_slope = st.selectbox("ST Slope", ["Up", "Flat", "Down"])
 
 if st.button("Predict"):
     raw_input = {
-        'Age' : age,
-        'restingBP' : restingBP,
-        'Cholestrol' : cholestrol,
-        'FastingBS' : fastingBS,
-        'MaxHR' : maxHR,
-        'OldPeak' : old_peak,
-        'Sex_' + sex: 1,
-        'ChestPainType' + chestPain : 1,
-        'RestingECG' + restingECG: 1,
-        'ExerciseAngina' +  exercise_angina: 1,
-        'ST_Slope' + st_slope: 1,
+        "Age": age,
+        "RestingBP": restingBP,
+        "Cholesterol": cholestrol,
+        "FastingBS": fastingBS,
+        "MaxHR": maxHR,
+        "Oldpeak": old_peak,
+        "Sex_M": 1 if sex == "M" else 0,
+        "ChestPainType_ATA": 1 if chestPain == "ATA" else 0,
+        "ChestPainType_NAP": 1 if chestPain == "NAP" else 0,
+        "ChestPainType_TA": 1 if chestPain == "TA" else 0,
+        "RestingECG_Normal": 1 if restingECG == "Normal" else 0,
+        "RestingECG_ST": 1 if restingECG == "ST" else 0,
+        "ExerciseAngina_Y": 1 if exercise_angina == "Y" else 0,
+        "ST_Slope_Up": 1 if st_slope == "Up" else 0,
+        "ST_Slope_Flat": 1 if st_slope == "Flat" else 0
     }
 
     input_df = pd.DataFrame([raw_input])
@@ -43,10 +47,11 @@ if st.button("Predict"):
             input_df[col] = 0
 
     input_df = input_df[columns]
+
     scaled_input = scaler.transform(input_df)
     prediction = model.predict(scaled_input)[0]
 
     if prediction == 1:
         st.error("High Risk of Heart Disease")
     else:
-        st.success("Low Risk of Hear Disease")
+        st.success("Low Risk of Heart Disease")
